6f6b4d6a037ccf3a93f6baa75ed3c0d982ad1e24
Daniel Náger Carpio <dnagerc@inscastellet.cat>
2022-11-17 13:23:37 +0100

    alguns canvis learning css

---
+++ learningByDoingCss/README.md
@@ -0,0 +1,2 @@
+In this folder, i will learn some tips of css from 
+https://css-tricks.com/snippets/css/
---
+++ projectes_react/just-scan-it/src/App.js
@@ -0,0 +1,10 @@
+import logo from './logo.svg';
+import './App.css';
+import Mostra from './components/Mostra';
+function App() {
+  return (
+    <Mostra/>
+  );
+}
+
+export default App;
